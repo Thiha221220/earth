8f06fd15400d55e86692424cead1b3dffab77ad0
heinhtetzaw4 <40501820@live.napier.ac.uk>
2022-04-05 15:06:04 +0630

    Adding Javdoc and comments in classes

--- src/main/java/com/napier/earth/CapitalCity.java
+++ src/main/java/com/napier/earth/CapitalCity.java
@@ -1,5 +1,11 @@
 package com.napier.earth;
 
+/**
+ * @author Thiha Htun Htun
+ * @version 4.0
+ * @since 1.0
+ */
+
 public class CapitalCity {
     private String Name;
 
@@ -7,17 +13,36 @@ public class CapitalCity {
 
     private Integer Population;
 
+    /**
+     * get capital city name
+     * @return Name
+     */
+
     public String getName() {
         return Name;
     }
+
+    /**
+     * get country
+     * @return Country
+     */
 
     public String getCountry() {
         return Country;
     }
 
+    /**
+     * get population
+     * @return Population
+     */
+
     public Integer getPopulation() {
         return Population;
     }
+
+    /**
+     *  Capital City Report Constructor
+     */
 
     public CapitalCity(String name, String country, Integer population) {
         Name = name;
--- src/main/java/com/napier/earth/City.java
+++ src/main/java/com/napier/earth/City.java
@@ -1,32 +1,60 @@
 package com.napier.earth;
 
-public class City {
-    private String Name;
+/**
+ * @author Yoon Shwe Lwin
+ * @version 4.0
+ * @since 1.0
+ */
 
+public class City {
+
+    private String Name;
 
     private String Country;
 
     private String District;
+
     private Integer Population;
+
+    /**
+     * get city name
+     * @return Name
+     */
 
     public String getName() {
         return Name;
     }
 
+    /**
+     * get country name
+     * @return Country
+     */
+
     public String getCountry() {
-
         return Country;
-
     }
+
+    /**
+     * get district name
+     * @return district
+     */
 
     public String getDistrict() {
         return District;
     }
 
+    /**
+     * get population
+     * @return Population
+     */
+
     public Integer getPopulation() {
         return Population;
     }
 
+    /**
+     *  City Report Constructor
+     */
 
     public City(String name, String country, String district, Integer population) {
         Name = name;
